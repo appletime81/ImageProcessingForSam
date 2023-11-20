ddea6b2f305b5d3fcc825182f11b13e2d9dfae17
appletime81 <itrimsl8696@gmail.com>
2023-11-20 18:24:32 +0800

    Update

--- binary_image.py
+++ binary_image.py
@@ -1,22 +1,43 @@
 import cv2
+from skimage.metrics import structural_similarity as ssim
 import numpy as np
 
-img1 = cv2.imread('Image/Input/10013993PTRS1/Testing_2/10013993PTRS1_TWNMLR02V6D00060D1_19_3_20231107102222.jpg')
-img2 = cv2.imread('Image/Golden/10013993PTRS1/Stream3-Golden.jpg')
-img_gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY); # 轉換前，都先將圖片轉換成灰階色彩
-img_gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY); # 轉換前，都先將圖片轉換成灰階色彩
-print(img_gray2.shape)
-ret1, output1 = cv2.threshold(img_gray1, 127, 255, cv2.THRESH_BINARY)
-ret2, output2 = cv2.threshold(img_gray2, 127, 255, cv2.THRESH_BINARY)
-output2 = cv2.adaptiveThreshold(img_gray1, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 23, 2)
-output3 = cv2.adaptiveThreshold(img_gray2, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 15, 2)
-dst = cv2.fastNlMeansDenoising(output3, None, 10, 15, 75)
+img1 = cv2.imread(
+    "Image/Input/10013993PTRS1/Testing_2/10013993PTRS1_TWNMLR02V6D00060D1_19_1_20231107102222_neg.png"
+)
+# img1 = cv2.imread(
+#     "Image/Input/10014010PTRU1/Testing_1/10014010PTRU1_NOSTRIPID_1_1_20231109054049.jpg"
+# )
+img2 = cv2.imread("Image/Golden/10013993PTRS1/Stream1-Golden.jpg")
 
+# ------------------------------- convert to gray --------------------------------
+img_gray1 = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
+img_gray2 = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+# --------------------------------------------------------------------------------
 
-# cv2.imshow('oxxostudio', img)
-# cv2.imshow('oxxostudio1', output1)
-cv2.imshow('oxxostudio2', output2)
-cv2.imshow('oxxostudio3', output3)
-cv2.imshow('oxxostudio4', dst)
+# ------------------------------------ binary ------------------------------------
+output2 = cv2.adaptiveThreshold(
+    img_gray1, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 23, 2
+)
+output3 = cv2.adaptiveThreshold(
+    img_gray2, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 23, 2
+)
+# --------------------------------------------------------------------------------
+
+# --------------------------------- filter noise ---------------------------------
+median1 = cv2.medianBlur(output2, 13)
+median2 = cv2.medianBlur(output3, 13)
+# --------------------------------------------------------------------------------
+
+# ----------------------------------- cal SSIM -----------------------------------
+ssim_med_1vs2 = ssim(median1, median2, data_range=median2.max() - median2.min())
+print(ssim_med_1vs2)
+# --------------------------------------------------------------------------------
+
+# cv2.imshow('ret002', output2)
+# cv2.imshow('ret003', output3)
+# cv2.imshow('ret_dst', dst)
+cv2.imshow("ret_median1", median1)
+cv2.imshow("ret_median2", median2)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
